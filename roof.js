7c2bd231f55256a2e01214518daafb1e283ff47a
Shasank2250 <71697524+Shasank2250@users.noreply.github.com>
2020-10-13 15:10:32 +0530

    Add files via upload

---
+++ roof.js
@@ -0,0 +1,24 @@
+class roof {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: true
+      }
+      this.x = x;
+      this.y = y;
+      
+      this.width = width;
+      this.height = height;
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      World.add(world, this.body);
+    }
+    display(){
+      var roofpos =this.body.position;
+      push();
+      translate(roofpos.x,roofpos.y);
+      rectMode(CENTER);
+      strokeWeight(3);
+      fill("gray");
+      rect(0, 0, this.width, this.height);
+      pop();
+    }
+  };
